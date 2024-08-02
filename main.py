e19c52c0b6a785652447185df7475c281492134c
Sillyman101 <danilsviridov3344@gmail.com>
2024-08-02 16:44:46 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+number_of_spent_hours = 1.5
+total_of_objectives = 12
+name_of_the_course = 'Python'
+average_time_spent = number_of_spent_hours / total_of_objectives
+print(name_of_the_course + ", всего задач:" + str(total_of_objectives) + ", затрачено часов: " + str(number_of_spent_hours) + ", среднее время выполнения " + str(average_time_spent) + " часа.")
